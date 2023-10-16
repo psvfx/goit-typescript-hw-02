b4fbf94b0b95323547df8a4793206530b40e82b0
psvfx <psvfxinvest@gmail.com>
2023-10-16 20:20:46 +0300

    exercises 3-8 have been added to the generic folder

--- src/generic/6.ts
+++ src/generic/6.ts
@@ -12,12 +12,20 @@ type User = {
   surname: string;
   email: string;
   password: string;
-}
+};
 
-function createOrUpdateUser(initialValues: User) {
+function createOrUpdateUser(initialValues: Partial<User>): User {
   // Оновлення користувача
+  const defaultUser: User = {
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+  };
+
+  return { ...defaultUser, ...initialValues };
 }
 
-createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
+createOrUpdateUser({ email: "user@mail.com", password: "password123" });
 
 export {};
